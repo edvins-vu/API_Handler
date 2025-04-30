edcb8e651082f4a832285f9275068196162c77b7
Edvins Vecverdins-Udris <edvins.vecverdins@estoty.com>
2025-04-30 08:50:50 +0300

    Pulled latest main

--- API_Tests/HandlerTests/APIManagerTests.cs
+++ API_Tests/HandlerTests/APIManagerTests.cs
@@ -3,15 +3,15 @@ namespace API_Tests.HandlerTests
 {
 	public class APIManagerTests
 	{
-		[Fact]
-		public void Test1()
-		{
-			// Arrange
-			//var handler = new APIManager();
-			//// Act
-			//var result = handler.SendRequest();
-			//// Assert
-			//Assert.Equal("Expected Result", result);
-		}
+		//[Fact]
+		//public void Test1()
+		//{
+		//	Arrange
+		//   var handler = new APIManager();
+		//	// Act
+		//	var result = handler.SendRequest();
+		//	// Assert
+		//	Assert.Equal("Expected Result", result);
+		//}
 	}
 }
